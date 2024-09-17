041e3bffd221247c34c5a6840bf09badc7f96851
tdv <tuduongvan2305@gmail.com>
2024-09-17 08:29:36 +0700

    jj

--- Bai01/TinhChuViDienTichHCN.java
+++ Bai01/TinhChuViDienTichHCN.java
@@ -2,6 +2,21 @@ package Bai01;
 
 public class TinhChuViDienTichHCN {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        if(args.length != 2){
+            System.out.println("Vui long nhap dung 2 tham so chieu dai va chieu rong");
+            return;
+        }
+        int width = Integer.parseInt(args[0]);
+        int height = Integer.parseInt(args[1]);
+
+        if ( width <= 0 || height <= 0){
+            System.out.println("Chieu dai va chieu rong phai la so nguyen duong");
+            return;
+        }
+
+        System.out.println("Chu vi "+ (2 *(width+height)));
+        System.out.println("Dien tich "+ (width*height));
+
+
     }
 }
