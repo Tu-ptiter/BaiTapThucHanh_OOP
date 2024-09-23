16705d96a7bf63ba3de4b787368c42430bcf8f9c
tdv <tuduongvan2305@gmail.com>
2024-09-23 16:03:52 +0700

    ms

---
+++ Bai02/DienTichChuViHCN.java
@@ -0,0 +1,10 @@
+package Bai02;
+
+public class DienTichChuViHCN {
+    public static void main(String[] args) {
+        Rectangle n = new Rectangle(32,21);
+
+        System.out.println( "Dien tich: "+n.getArea());
+        System.out.println("Chu vi: "+n.getPerimeter());
+    }
+}
